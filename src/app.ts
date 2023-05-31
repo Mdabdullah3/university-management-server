c4cc4e87ececa4eee51459106d01015f02c4a405
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2023-05-31 21:50:17 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request,  } from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 
 const app: Application = express()
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }))
 
 // Testing
 
-app.get('/', (req: Request, res: any) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Working SuccesFull!')
 })
 
