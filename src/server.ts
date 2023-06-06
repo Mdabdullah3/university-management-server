42bba8858d2fedc927958365f175f9e80179f8bf
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2023-06-06 20:27:44 +0600

    new changes

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
+import { errorlogger, logger } from './shared/logger'
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('Database connnect succesfull')
+    logger.info('Database connnect succesfull')
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`)
+      logger.info(`Example app listening on port ${config.port}`)
     })
   } catch (err) {
-    console.log('falied connect')
+    errorlogger.error('falied connect', err)
   }
 }
 
